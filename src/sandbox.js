4002ab044475b03936c312edb1a319524b238a74
pcurich <curichpedro@gmail.com>
2022-08-02 14:56:12 -0500

    fin

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,38 @@
-import { displayLog } from "./utils";
+import { updateDisplay } from "./utils";
 import { fromEvent } from "rxjs";
-import { tap, map, filter } from "rxjs/operators";
+import { map, tap, pairwise } from "rxjs/operators";
 
-export default () => {};
+export default () => {
+  //pairwise: Se encarga de emitir eventos consecutivos del dato actual y el anterior sin necesidad de usar variables externas
+
+  /** start coding */
+  const progressBar = document.getElementById("progress-bar");
+  const docElement = document.documentElement;
+  const updateProgressBar = (percentage) => {
+    progressBar.style.width = `${percentage}%`;
+  };
+
+  //observable that returns scroll (from top) on scroll events
+  const scroll$ = fromEvent(document, "scroll").pipe(
+    map(() => docElement.scrollTop),
+    tap((evt) => console.log("[scroll]: ", evt)),
+    pairwise(),
+    tap(([previous, current]) => {
+      updateDisplay(current > previous ? "DESC" : "ASC");
+    }),
+    map(([previous, current]) => current)
+  );
+
+  //observable that returns the amount of page scroll progress
+  const scrollProgress$ = scroll$.pipe(
+    map((evt) => {
+      const docHeight = docElement.scrollHeight - docElement.clientHeight;
+      return (evt / docHeight) * 100;
+    })
+  );
+
+  //subscribe to scroll progress to paint a progress bar
+  const subscription = scrollProgress$.subscribe(updateProgressBar);
+
+  /** end coding */
+};
