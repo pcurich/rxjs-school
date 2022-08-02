0613f410dba0062e05f37400e88542d9a4fded5e
pcurich <curichpedro@gmail.com>
2022-08-02 15:16:13 -0500

    update

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,38 @@
-import { displayLog } from "./utils";
+import { updateDisplay } from "./utils";
 import { fromEvent } from "rxjs";
-import { tap, map, filter } from "rxjs/operators";
+import { map, tap, share } from "rxjs/operators";
 
-export default () => {};
+export default () => {
+  /** start coding */
+
+  const progressBar = document.getElementById("progress-bar");
+  const docElement = document.documentElement;
+
+  //function to update progress bar width on view
+  const updateProgressBar = (percentage) => {
+    progressBar.style.width = `${percentage}%`;
+  };
+
+  //observable that returns scroll (from top) on scroll events
+  const scroll$ = fromEvent(document, "scroll").pipe(
+    map(() => docElement.scrollTop),
+    tap((evt) => console.log("[scroll]: ", evt))
+  );
+
+  //observable that returns the amount of page scroll progress
+  const scrollProgress$ = scroll$.pipe(
+    map((evt) => {
+      const docHeight = docElement.scrollHeight - docElement.clientHeight;
+      return (evt / docHeight) * 100;
+    }),
+    share()
+  );
+
+  //subscribe to scroll progress to paint a progress bar
+  const subscription = scrollProgress$.subscribe(updateProgressBar);
+
+  //subscribe to display scroll progress percentage
+  const subscription2 = scrollProgress$.subscribe((val) => updateDisplay(`${Math.floor(val)} %`));
+
+  /** end coding */
+};
