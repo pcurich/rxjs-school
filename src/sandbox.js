fa44939805dae6a69cfca6d2e4d456691ef35c00
pcurich <curichpedro@gmail.com>
2022-08-02 13:14:12 -0500

    fin

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,17 @@
 import { displayLog } from "./utils";
 import { fromEvent } from "rxjs";
-import { tap, map, filter } from "rxjs/operators";
+import { tap, map, filter, first, scan, takeWhile, startWith, endWith } from "rxjs/operators";
 
-export default () => {};
+export default () => {
+  const grid = document.getElementById("grid");
+
+  // startWith: modifica el observable para emitir ciertos valores de forma previa a cualquier evento del observable origianl
+  const clickStartWithe$ = fromEvent(grid, "click").pipe(
+    map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(([col, _]) => col != 0),
+    tap((val) => console.log(`cell: [${val}]`)),
+    startWith("Grid dimensions: ", "10x10"),
+    endWith("Game finish", "Bye")
+  );
+  clickStartWithe$.subscribe((data) => displayLog(`${data}`));
+};
