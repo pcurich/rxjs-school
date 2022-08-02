693f01980e38373b8b35c888e606e620b592b127
pcurich <curichpedro@gmail.com>
2022-08-02 11:17:28 -0500

    fin

--- src/create.js
+++ src/create.js
@@ -1,3 +1,26 @@
 import { displayLog } from "./utils";
+import { of, range } from "rxjs";
 
-export default () => {};
+export default () => {
+  //of permite emitir una secuencia variable de objetos de distinta tipologia
+  const source1 = of(1, 2, 3, 4, 5, 6);
+  const subscribe1 = source1.subscribe(displayLog);
+
+  const source2 = of(
+    [1, 2, 3, 4, 5, 6],
+    "Hello World",
+    {
+      foo: "bar",
+      id: 1,
+    },
+    function sayHello() {
+      return "HI!";
+    }
+  );
+
+  const subscribe2 = source2.subscribe(displayLog);
+
+  //RANGE: secuencia ordenada de numero, desde y cuantos
+  const source3 = range(3, 10);
+  const subscribe3 = source3.subscribe(displayLog);
+};
