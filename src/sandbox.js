9b742c58443e7565167d74de50b77d5f41f4ebcd
pcurich <curichpedro@gmail.com>
2022-08-02 13:07:45 -0500

    fin

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,41 @@
 import { displayLog } from "./utils";
 import { fromEvent } from "rxjs";
-import { tap, map, filter } from "rxjs/operators";
+import { tap, map, filter, first, scan, takeWhile, reduce } from "rxjs/operators";
 
-export default () => {};
+export default () => {
+  const grid = document.getElementById("grid");
+
+  // reduce: recibe un acumulador y el valor actual y emite un unico evento al completarse el stream
+  const clickReduce$ = fromEvent(grid, "click").pipe(
+    map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(([col, _]) => col != 0),
+    tap((val) => console.log(`cell: [${val}]`)),
+    reduce(
+      (accumulated, current) => {
+        return {
+          clicks: accumulated.clicks + 1,
+          cells: [...accumulated.cells, current],
+        };
+      },
+      { clicks: 0, cells: [] }
+    )
+  );
+  clickReduce$.subscribe((data) => displayLog(`${data.clicks} clicks: ${JSON.stringify(data.cells)}`));
+
+  // reduce: recibe un acumulador y el valor actual y emite varios eventos al completarse el stream
+  const clickScan$ = fromEvent(grid, "click").pipe(
+    map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    takeWhile(([col, _]) => col != 0),
+    tap((val) => console.log(`cell: [${val}]`)),
+    scan(
+      (accumulated, current) => {
+        return {
+          clicks: accumulated.clicks + 1,
+          cells: [...accumulated.cells, current],
+        };
+      },
+      { clicks: 0, cells: [] }
+    )
+  );
+  clickScan$.subscribe((data) => displayLog(`${data.clicks} clicks: ${JSON.stringify(data.cells)}`));
+};
