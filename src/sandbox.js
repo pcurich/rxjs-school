50abffdd9718b70f15e2b983166d8b6e3efce0fb
pcurich <curichpedro@gmail.com>
2022-08-03 05:07:08 -0500

    fin

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,10 +1,15 @@
 import { updateDisplay } from "./utils";
-import { fromEvent } from "rxjs";
+import { fromEvent, Subject, BehaviorSubject } from "rxjs";
 import { map, tap, share } from "rxjs/operators";
 
 export default () => {
   /** start coding */
 
+  //subject es un tipo especial de observable con 3 propiedades:
+  //* Puede hacer multi cast de sus valores hacia varios observadores, osea es un hot observable
+  //* Ademas de ser un observable tambien es un observador, tiene los metodos subscribe y pipe,
+  //tambien tiene los metodos next, complete y error
+  //* Es que actua como un distribuidor (2 anteriores)
   const progressBar = document.getElementById("progress-bar");
   const docElement = document.documentElement;
 
@@ -24,15 +29,24 @@ export default () => {
     map((evt) => {
       const docHeight = docElement.scrollHeight - docElement.clientHeight;
       return (evt / docHeight) * 100;
-    }),
-    share()
+    })
+    // share()
   );
 
+  //   const scrollProgressHot$ = new Subject();
+  const scrollProgressHot$ = new BehaviorSubject(-3);
+  scrollProgress$.subscribe(scrollProgressHot$);
+
   //subscribe to scroll progress to paint a progress bar
-  const subscription = scrollProgress$.subscribe(updateProgressBar);
+  const subscription = scrollProgressHot$.subscribe(updateProgressBar);
 
   //subscribe to display scroll progress percentage
-  const subscription2 = scrollProgress$.subscribe((val) => updateDisplay(`${Math.floor(val)} %`));
+  const subscription2 = scrollProgressHot$.subscribe((val) => updateDisplay(`${Math.floor(val)} %`));
 
+  //   scrollProgressHot$.next(-1); //estoy forzando el que tenga variables iniciales
+  //const scrollProgressHot$ = new BehaviorSubject(-1);
+
+  ///se guarda el ultimo evento emitido
+  console.log("Scroll inirial state: ", scrollProgressHot$.value);
   /** end coding */
 };
